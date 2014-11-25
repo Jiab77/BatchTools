3c99e723ac076bdaa68b4bbb14fd62161c952b6d
Jonathan Barda <jonathan.barda@gmail.com>
2014-11-25 09:22:04 +0100

    Update md5sums.bat
    Changed variable name

--- md5sums.bat
+++ md5sums.bat
@@ -3,13 +3,13 @@ for %%a in (cls echo) do %%a.
 setlocal EnableDelayedExpansion
 
 set "list=%*"
-set "md5_path=%~dp0" & set md5_path=!basePath:~0,-1!\MD5
+set "md5Path=%~dp0" & set md5_path=!basePath:~0,-1!\MD5
 
 echo DATA PASSED: !list!
 
 for %%m in (!list!) do (
 	title Hashing [%%~nxm]...
-	call %md5_path%\md5sums.exe -u -e "%%~m" > "%%~m.md5"
+	call %md5Path%\md5sums.exe -u -e "%%~m" > "%%~m.md5"
 )
 
 pause
