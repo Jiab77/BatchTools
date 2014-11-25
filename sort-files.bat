8148e9a780378c7f1b8065709d3919f762bb29c8
Jiab77 <jonathan.barda@gmail.com>
2014-11-25 09:25:05 +0100

    Few sorting Batch Scripts

---
+++ sort-files.bat
@@ -0,0 +1,10 @@
+@echo off
+cls
+for %%L in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
+	for /F "delims=;" %%D in ('dir /B/A-D %CD%\%%L*') do (
+		if exist "%CD%\%%L*" (
+			if not exist %CD%\%%L mkdir %CD%\%%L
+			(move "%CD%\%%D" "%CD%\%%L") && echo Moving [%%D] to [%CD%\%%L] || echo Error. Can't move [%%D] to [%CD%\%%L]
+		)
+	)
+)
